4eea3e1eba1e894d54a89d13247e085fcdaae45a
Makshas007 <sakshamdhamija007@gmail.com>
2026-07-19 02:22:32 +0530

    Update TreePanel.jsx

--- frontend/src/components/TreePanel.jsx
+++ frontend/src/components/TreePanel.jsx
@@ -32,10 +32,10 @@ function flattenTree(node, edges, currId) {
       type: 'historyEdge',
     }
     if (hasSameJob) {
-      edgeObj.style = { stroke: '#00ff00', strokeWidth: 3, strokeDasharray: 'none' }
+      edgeObj.style = { stroke: '#7fffd4', strokeWidth: 3, strokeDasharray: 'none' }
       edgeObj.markerEnd = {
         type: MarkerType.ArrowClosed,
-        color: '#00ff00',
+        color: '#7fffd4',
         width: 15,
         height: 15,
       }
